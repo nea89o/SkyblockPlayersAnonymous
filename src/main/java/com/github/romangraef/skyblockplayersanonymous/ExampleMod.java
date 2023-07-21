4f8ca665888f10504d2995e9bed4b95ea60554c3
GitHub Action <action@github.com>
2023-07-21 10:15:53 +0000

    Initialize template repository proper
    Set package name to com.github.romangraef.skyblockplayersanonymous
    Set project name to SkyblockPlayersAnonymous
    Set modid to skyblockplayersanonymous

--- src/main/java/com/github/romangraef/skyblockplayersanonymous/ExampleMod.java
+++ src/main/java/com/github/romangraef/skyblockplayersanonymous/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.romangraef.skyblockplayersanonymous;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "skyblockplayersanonymous", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
